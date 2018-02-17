862bd17c6f294340a1eeed391499c2f36a328e28
Dan Cancelmo <dcancelm@u.rochester.edu>
2018-02-17 16:26:44 -0500

    Form data

--- cgi-bin/issue_create.py
+++ cgi-bin/issue_create.py
@@ -3,12 +3,27 @@
 
 import cgitb
 import sqlite3
+import cgi
 
 
 cgitb.enable()
 
 conn = sqlite3.connect('dejavu.db')
 c = conn.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS issues(iID INT PRIMARY KEY NOT NULL, user VARCHAR(30) NOT NULL, name VARCHAR(300) NOT NULL, description TEXT, solution TEXT NOT NULL, attempts TEXT, FOREIGN KEY (user) REFERENCES users(username))')
+c.execute('CREATE TABLE IF NOT EXISTS issues(iID INT PRIMARY KEY NOT NULL, user VARCHAR(30) NOT NULL, name VARCHAR(300) NOT NULL, description TEXT NOT NULL, solution TEXT NOT NULL, attempts TEXT, FOREIGN KEY (user) REFERENCES users(username))')
 c.execute('CREATE TABLE IF NOT EXISTS tags(iID INT NOT NULL, tag VARCHAR(50) NOT NULL, FOREIGN KEY (iID) REFERENCES issues(iID), CONSTRAINT pk_tags PRIMARY KEY (iID, tag))')
 c.execute('CREATE TABLE IF NOT EXISTS users(username VARCHAR(30) PRIMARY KEY NOT NULL, password CHAR(64) NOT NULL, timeCreated VARCHAR(26) NOT NULL)')
+
+form = cgi.FieldStorage()
+
+user = str(form['username'].value)
+name = str(form['name'].value)
+description = str(form['description'].value)
+solution = str(form['solution'].value)
+if 'attempts' in form:
+    attempts = str(form['attempts'].value)
+else:
+    attempts = None
+tagsList = str(form['tags'].value)
+tagArr = tagsList.split(", ")
+

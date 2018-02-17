ececfdc512c1e2d0865c3823454d1c74e917805d
Dan Cancelmo <dcancelm@u.rochester.edu>
2018-02-17 18:21:27 -0500

    UUID and tags

--- cgi-bin/issue_create.py
+++ cgi-bin/issue_create.py
@@ -4,19 +4,21 @@
 import cgitb
 import sqlite3
 import cgi
+import uuid
 
 
 cgitb.enable()
 
 conn = sqlite3.connect('dejavu.db')
 c = conn.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS issues(iID INT PRIMARY KEY NOT NULL, user VARCHAR(30) NOT NULL, name VARCHAR(300) NOT NULL, description TEXT NOT NULL, solution TEXT NOT NULL, attempts TEXT, FOREIGN KEY (user) REFERENCES users(username))')
+c.execute('CREATE TABLE IF NOT EXISTS issues(iID VARCHAR(32) PRIMARY KEY NOT NULL, user VARCHAR(30) NOT NULL, name VARCHAR(300) NOT NULL, description TEXT NOT NULL, solution TEXT NOT NULL, attempts TEXT, FOREIGN KEY (user) REFERENCES users(username))')
 c.execute('CREATE TABLE IF NOT EXISTS tags(iID INT NOT NULL, tag VARCHAR(50) NOT NULL, FOREIGN KEY (iID) REFERENCES issues(iID), CONSTRAINT pk_tags PRIMARY KEY (iID, tag))')
 c.execute('CREATE TABLE IF NOT EXISTS users(username VARCHAR(30) PRIMARY KEY NOT NULL, password CHAR(64) NOT NULL, timeCreated VARCHAR(26) NOT NULL)')
 
 form = cgi.FieldStorage()
 
-#TODO generate a uID
+uuidObj = uuid
+uID = uuidObj.uuid4()
 user = str(form['username'].value)
 name = str(form['name'].value)
 description = str(form['description'].value)
@@ -32,8 +34,44 @@ tagArr = tagsList.split(", ")
 try:
     c.execute('INSERT INTO issues (uID, user, name, description, solution, attempts) VALUES (?, ?, ?, ?, ?, ?)',
               [uID, user, name, description, solution, attempts])
+    try:
+        for currTag in tagArr:
+            c.execute('INSERT INTO tags(uID, tag) VALUES (?, ?)', [uID, currTag])
+            print 'Content-Type: text/html'
+            print "Location: ../index.html"
+            print
+
+    except sqlite3.IntegrityError:
+        print 'Content-Type: text/html'
+        print
+        print '''<html>
+                                <head>
+                                    <title>Error</title>
+                                    <script type = "text/javascript">
+                                    	alert("Error. Failure to create tags entry.");
+                                    	window.location.href = "../index.html";
+                                    </script>
+                                </head>
+                                <body>
+                                </body>
+                            </html>
+                            '''
 # else:
 except sqlite3.IntegrityError:
+    print 'Content-Type: text/html'
+    print
+    print '''<html>
+                        <head>
+                            <title>Error</title>
+                            <script type = "text/javascript">
+                            	alert("Error. Failure to create issue entry.");
+                            	window.location.href = "../index.html";
+                            </script>
+                        </head>
+                        <body>
+                        </body>
+                    </html>
+                    '''
 
 conn.commit()
 conn.close()

90bfebe83d9a20530704bd61a603fa3dbed081e8
Dan Cancelmo <dcancelm@u.rochester.edu>
2018-03-29 15:33:44 -0400

    Login and account create buttons match

--- account_create.php
+++ account_create.php
@@ -29,10 +29,10 @@
 					<input type="password" name="passwordConfirm"  id="passConfirm" placeholder = "Confirm Password" required>
 				</div>
 				<div class="row formQuestion">
-					<button type="submit" id="submit"> Create Account </button>
+					<button class="btn btn-success" type="submit" id="submit"> Create Account </button>
 				</div>
 				<div class="row formQuestion">
-					<h4><a href="login.php">Go back to login</a></h4>
+					<button class="btn btn-sm btn-info" id="loginReturn" onclick="window.location.replace('login.php')">Go back to login</button>
 				</div>
 				<div class="row formQuestion">
 					<h6 id="passMatch"></h6>
